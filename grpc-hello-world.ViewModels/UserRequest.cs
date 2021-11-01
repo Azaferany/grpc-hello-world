a3b9f053d607958becac9bb83075db4a00ae5076
ali <ali.zaferany79@gmail.com>
2021-11-01 21:50:01 +0330

    add ViewModels Proj

---
+++ grpc-hello-world.ViewModels/UserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using ProtoBuf;
+
+namespace grpc_hello_world.ViewModels
+{
+    [ProtoContract]
+    public class UserRequest
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+    }
+}
---
+++ grpc-hello-world.ViewModels/UserResult.cs
@@ -0,0 +1,17 @@
+﻿using ProtoBuf;
+
+namespace grpc_hello_world.ViewModels
+{
+    [ProtoContract]
+    public class UserResult
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+        
+        [ProtoMember(2)]
+        public string UserName { get; set; }
+        
+        [ProtoMember(3)]
+        public string Email { get; set; }
+    }
+}
